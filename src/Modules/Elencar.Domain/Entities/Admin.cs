6aa5e10275b2a9d98e2b347959d99c87ac0380e3
Evelyn15 <evelynkatarine@gmail.com>
2020-10-10 17:16:19 -0300

    Classe admin

---
+++ src/Modules/Elencar.Domain/Entities/Admin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elencar.Domain.Entities
+{
+    public class Admin
+    {
+        public Admin(string name)
+
+        {
+            Id = Guid.NewGuid();
+            Name = name;       
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+
+
+
+        public bool IsValid()
+        {
+            var valid = true;
+
+            if (string.IsNullOrEmpty(Name))
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
+    }
+
+    
+
+}

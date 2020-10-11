89b117d69724873bca7e78afc871ee61fb70cd87
Evelyn15 <evelynkatarine@gmail.com>
2020-10-10 22:24:21 -0300

    Repositorio genero e reserva

--- src/Modules/Elencar.Domain/Entities/Reservation.cs
+++ src/Modules/Elencar.Domain/Entities/Reservation.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Elencar.Domain.Entities
 {
-    class Reservation
+    public class Reservation
     {
         public Reservation(DateTime start, DateTime end, List<Genre> genres, Producer producer)
         {
---
+++ src/Modules/Elencar.Domain/Interfaces/Repositories/IReservationRepository.cs
@@ -0,0 +1,17 @@
+﻿using Elencar.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elencar.Domain.Interfaces.Repositories
+{
+    public interface IReservationRepository
+    {
+        int Insert(Reservation reservation);
+        Task<Reservation> GetByIdAsync(int id);
+        IEnumerable<Reservation> Get();
+        void Delete(Reservation reservation);
+        int Update(Reservation reservation);
+    }
+}
